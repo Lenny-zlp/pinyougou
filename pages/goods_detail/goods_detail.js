fe2c346465e2cfe646ee1e8417ac8feac5e57781
Lenny_zlp <704792246@qq.com>
2020-05-19 18:24:39 +0800

    加入购物车

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -7,9 +7,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{},
+    // 商品是否被收藏
+    isCollect:false
   },
-
+// 商品对象
+GoodsInfo: {},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -18,11 +21,61 @@ Page({
     console.log(goods_id)
     this.getGoodsDetail(goods_id)
   },
+
+    // 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const res = await request({url:"/goods/detail",data:{goods_id}})
+    this.GoodsInfo = res.data.message;
+    // 1 获取缓存中的商品收藏的数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 2 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
+    this.setData({
+      goodsObj:{
+        goods_name:res.data.message.goods_name,
+        goods_price:res.data.message.goods_price,
+        goods_introduce:res.data.message.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics:res.data.message.pics
+      },
+      isCollect
+    })
   },
-  // 获取商品详情数据
 
+  // 点击轮播图 放大预览
+  handlePrevewImage(e) {
+    const urls = this.GoodsInfo.pics.map(v=>v.pics_mid)
+    // console.log(e)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接
+      urls: urls // 需要预览的图片http链接列表
+    })
+  },
+
+  // 点击加入购物车
+  handleCartAdd() {
+    // 获取缓存中的购物车数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 判断该商品是否存在于购物车数组中
+    let index = cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    if(index === -1) {
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      this.GoodsInfo.checked = true;
+      cart.push(this.GoodsInfo);
+    }else {
+      // 已存在
+      cart[index].num++;
+    }
+    // 本地化
+    wx.setStorageSync("cart", cart);
+    // 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
